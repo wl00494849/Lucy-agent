1e8fa793d4452b2fd95f4a5f423db78ba1bf0aad
zhung1oz7 <wl00494849@gmail.com>
2025-09-04 17:44:29 +0800

    update

--- personl_test.py
+++
@@ -1,9 +0,0 @@
-from src.env import load_env
-from src.LLMs import LLMs
-
-load_env()
-
-while True:
-        prompt = input("請輸入問題：")
-        gpt = LLMs()
-        gpt.request(LLMs.LineBot_Requset(message=prompt,userID=""))
